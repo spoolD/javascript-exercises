74dd0d1fbe43013d18dff5850e1fe7b81a405385
Davy Sproule <dsproule@vt.edu>
2022-08-12 07:45:31 -0400

    Solve helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // Initialize String
+    finalString = '';
 
+    // Error check
+    if (num < 0){
+        return 'ERROR';
+    }
+    if (num === 0){
+        return '';
+    }
+
+    // Repeating string num times
+    for (let i=0; i<num; i++){
+        finalString += string;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
