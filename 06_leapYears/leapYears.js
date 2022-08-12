e44386f6d45b7e8677f9e458568406da870e8028
Davy Sproule <dsproule@vt.edu>
2022-08-12 09:44:03 -0400

    Solve leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //Leap year if divisible by 4
+    if (year % 4 === 0){
+        //Leap year if divisible by 100 and 400
+        if(year % 100 === 0 && year % 400 === 0){
+            return true;
+        }
+        //Not a leap year if divisible by 100 but not 400
+        else if (year % 100 === 0 && year % 400 !== 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
