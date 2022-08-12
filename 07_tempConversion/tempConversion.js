797b3fad7c04a023e3cea5fd562f556bf758631a
Davy Sproule <dsproule@vt.edu>
2022-08-12 10:08:57 -0400

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,30 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  //Conversion
+  let tempC = (5/9) * (tempF - 32);
 
+  //Don't round if integer
+  if(Number.isInteger(tempC)){
+    return tempC;
+  }
+
+  //Otherwise round to nearest tenth
+  else{
+    return Math.round(tempC*10)/10;
+  }
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+  //Conversion
+  let tempF = tempC * (9/5) + 32;
 
+  //Rounding
+  if(Number.isInteger(tempF)){
+    return tempF;
+  }
+  
+  else{
+    return Math.round(tempF*10)/10;
+  }
 };
 
 // Do not edit below this line
