02a2e5af25226ba8963b2d76c17fcbb293880cef
Davy Sproule <dsproule@vt.edu>
2022-08-12 09:17:39 -0400

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumNumbers = 0;
 
+    //Error checking
+    if (num1 < 0 || num2 < 0 || typeof(num1)!='number' || typeof(num2)!='number'){
+        return 'ERROR';
+    }
+    
+    //Assign variables based on values
+    let numLow = num1;
+    let numHigh = num2;
+    if (num2 < num1){
+        numLow = num2;
+        numHigh = num1;
+    }
+
+    //Sum values
+    for(i=numLow; i<=numHigh; i++){
+        sumNumbers += i;
+    }
+    return sumNumbers;
 };
 
 // Do not edit below this line
