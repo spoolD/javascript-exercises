1c8c933b5ba6065d0b5937ed4435a695e1e6c89e
Davy Sproule <dsproule@vt.edu>
2022-08-12 08:52:44 -0400

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeMe) {
+    //Iterate through each entry to remove
+    for (const arg of removeMe){
+        // Iterate through array and remove each instance
+        for (let i=0; i<array.length; i++){
+            if (array[i] === arg){
+                array.splice(i,1)
+            }
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
