ec5bee0930ba37f80e7df4fe3e792910b8685e2e
Davy Sproule <dsproule@vt.edu>
2022-09-05 15:09:07 -0400

    Solve 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumValue = 0;
+  for (let i=0; i<array.length; i++){
+    sumValue += array[i];
+  }
+  return sumValue
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array[0];
+  for (let i=1; i<array.length; i++){
+    product *= array[i];
+  } 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  if (value === 0){
+    return 1;
+  }
+  
+	let total = value;
+  while (value > 1){
+    value -= 1;
+    total *= value;
+  }
+  return total;
 };
 
 // Do not edit below this line
