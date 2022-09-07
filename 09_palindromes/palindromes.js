a5f40de25b0d2f7b2ef498b5cec46e2a82340ec6
Davy Sproule <dsproule@vt.edu>
2022-09-06 21:00:38 -0400

    Initial progress on 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //Convert to array and filter out spaces and punctuation with regex
+    const stringFiltered = Array.from(string.toLowerCase()).filter((element) => {
+        /[a-z]/.test(element)
+    })
 
+    //Join array back into string
+    const stringForward = stringFiltered.join("");
+
+    //Turn string into array, reverse, and rejoin for reversed string
+    const stringBackward = [...stringForward].reverse().join("");
+
+    return stringForward === stringBackward;
 };
 
 // Do not edit below this line
