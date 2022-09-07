4e75376eed9bae92cd3472ffd2ce72a7dbc2a661
Davy Sproule <dsproule@vt.edu>
2022-09-07 15:57:55 -0400

    Solved 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    let initValues = [1, 1];
+    let i = 2;
+    if (val <= 0){
+        return 'OOPS'
+    }
+    
+    while (i < val){
+        initValues.push(initValues[i-1] + initValues[i-2]);
+        i++;
+    }
 
+    return initValues[val-1]
 };
 
 // Do not edit below this line
