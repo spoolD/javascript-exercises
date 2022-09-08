2b3d7f87758cecfda6a18acab0cf19d05f7f77c6
Davy Sproule <dsproule@vt.edu>
2022-09-08 19:52:38 -0400

    Solve 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people){
+    //Sub current year for year of death if still living
+    people.forEach(person => {
+        const today = new Date();
+        if (!('yearOfDeath' in person)){
+            person.yearOfDeath = today.getFullYear();
+        }
+    });
 
+    //Order object by years lived
+    const ordered = people.sort(function(a, b) {
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
+        return last > next ? -1 : 1;
+    });
+    return ordered[0];
 };
 
 // Do not edit below this line
